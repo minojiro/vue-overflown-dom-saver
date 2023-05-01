e3a2efeb2dc06ffdb129c8c1c4a5c9c280ee28e7
minojiro <minojiro@users.noreply.github.com>
2023-05-01 13:29:37 +0900

    chore: example app

---
+++ example/app.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { OverflownDomSaver } from "vue-overflown-dom-saver";
+</script>
+
+<template>
+  <div>
+    <h1>OverflownDomSaver example</h1>
+    <ul>
+      <li v-for="(_, i) in Array(100)" :key="i">
+        <OverflownDomSaver>
+          <ListItem :itemNumber="i" />
+        </OverflownDomSaver>
+      </li>
+    </ul>
+  </div>
+</template>
---
+++ example/components/ListItem.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps({
+  itemNumber: {
+    type: Number,
+    required: true,
+  },
+});
+const height = [Math.random() * 100 + 100, "px"].join("");
+</script>
+<template>
+  <div :style="{ height }">
+    <p>item: {{ itemNumber }}</p>
+  </div>
+</template>
+
+<style scoped>
+div {
+  background: #fcc;
+}
+</style>
